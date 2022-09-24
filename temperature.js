395ae1d20fe4b3e5ca46f4baf16f6213d4eacaef
knika-starkey <knika787@gmail.com>
2022-09-24 13:23:21 +0300

    avarege temp

--- temperature.js
+++ temperature.js
@@ -1,7 +1,9 @@
 let temp = [12, 5, -3, -9, 2, 15, 0];
 let hotdays = 0;
 let colddays = 0;
+let sum = 0;
 for (let i = 0; i < temp.length; i++) {
+  sum += temp[i];
   if (temp[i] > 0) {
     alert(temp[i] + "+");
     hotdays++;
@@ -11,3 +13,4 @@ for (let i = 0; i < temp.length; i++) {
   }
 }
 alert(`У цьому місяці ${hotdays} теплих днів та ${colddays} холодних днів`);
+alert(`Середня температура дорівнює ${sum / temp.length}`);
